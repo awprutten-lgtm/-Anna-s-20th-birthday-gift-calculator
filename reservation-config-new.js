f1f606cf21b6f1f50ea9e93fa7c47930cb91f689
awprutten-lgtm <awprutten@gmail.com>
2026-07-31 11:28:58 +0200

    Add files via upload

---
+++ reservation-config-new.js
@@ -0,0 +1,4 @@
+// Vul alleen databaseURL in nadat je Firebase hebt ingesteld.
+window.RESERVATION_CONFIG = {
+  databaseURL: "https://console.firebase.google.com/project/anna-birthday-gift-calculator/database/anna-birthday-gift-calculator-default-rtdb/data/~2F"
+};
