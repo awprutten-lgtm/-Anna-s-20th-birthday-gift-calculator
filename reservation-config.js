74cd7fc45dd1bdaa93b74b20706a8711ca5d11b1
awprutten-lgtm <awprutten@gmail.com>
2026-07-31 22:33:01 +0200

    Delete reservation-config.js

--- reservation-config.js
+++
@@ -1,4 +0,0 @@
-// Vul alleen databaseURL in nadat je Firebase hebt ingesteld.
-window.RESERVATION_CONFIG = {
-  databaseURL: "https://anna-birthday-gift-calculator-default-rtdb.europe-west1.firebasedatabase.app/"
-};
