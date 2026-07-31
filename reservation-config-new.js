c6c0f8c2a3a52af9cf2290938f4cf8dc1aea8ada
awprutten-lgtm <awprutten@gmail.com>
2026-07-31 11:39:09 +0200

    Update reservation-config-new.js

--- reservation-config-new.js
+++ reservation-config-new.js
@@ -1,4 +1,4 @@
 // Vul alleen databaseURL in nadat je Firebase hebt ingesteld.
 window.RESERVATION_CONFIG = {
-  databaseURL: "https://console.firebase.google.com/project/anna-birthday-gift-calculator/database/anna-birthday-gift-calculator-default-rtdb/data/~2F"
+  databaseURL: "https://anna-birthday-gift-calculator-default-rtdb.europe-west1.firebasedatabase.app/"
 };
